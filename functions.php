be7b8f87dde9cbdfade9efc05c34ab11d7fb1e86
longlake2 <sites@emailhart.net>
2018-01-14 13:06:44 -0500

    inital set up

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Include CSS files 
+ */
+function theme_enqueue_scripts() {
+        wp_enqueue_style( 'Font_Awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+        wp_enqueue_style( 'Bootstrap_css', get_template_directory_uri() . '/node_modules/mdbootstrap/css/bootstrap.min.css' );
+        wp_enqueue_style( 'MDB', get_template_directory_uri() . '/node_modules/mdbootstrap/css/mdb.min.css' );
+        wp_enqueue_style( 'Style', get_template_directory_uri() . '/style.css' );
+        wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/node_modules/mdbootstrap/js/jquery-3.2.1.min.js', array(), '2.2.3', true );
+        wp_enqueue_script( 'Tether', get_template_directory_uri() . '/node_modules/mdbootstrap/js/popper.min.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'Bootstrap', get_template_directory_uri() . '/node_modules/mdbootstrap/js/bootstrap.min.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'MDB', get_template_directory_uri() . '/node_modules/mdbootstrap/js/mdb.min.js', array(), '1.0.0', true );
+
+        }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
+
+
+
+             
+/**
+ * Setup Theme
+ */
+function MDB4wp_setup() {
+  // Navigation Menus
+  register_nav_menus(array(
+    'navbar' => __( 'Navbar Menu')
+    ));
+  // Add featured image support
+    add_theme_support('post-thumbnails');
+    add_image_size('main-full', 1078, 516, false); // main post image in full width
+  }
+  add_action('after_setup_theme', 'MDB4wp_setup');
+                                     
+
+/**
+ * Include external files
+ */
+require_once('inc/mdb_bootstrap_navwalker.php');
+
+
+?>
+
+
+
+
